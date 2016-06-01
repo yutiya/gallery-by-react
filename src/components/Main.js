c2560a704586b811566d3df8a889f38b84730814
Meone <18685258712@163.com>
2016-06-02 04:54:38 +0800

    success

--- src/components/Main.js
+++ src/components/Main.js
@@ -96,6 +96,38 @@ class ImgFigure extends React.Component {
   }
 }
 
+class ControllerUnit extends React.Component {
+  handleClick(e) {
+
+    //如果点击的是当前正在选中态的按钮，则翻转图片，否则将对应的图片居中
+    if (this.props.arrange.isCenter) {
+      this.props.inverse();
+    } else {
+      this.props.center();
+    }
+
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  render() {
+
+    var controllerUnitClassName = 'controller-unit';
+
+    // 如果对应的是居中的图片，显示控制按钮的居中态
+    if (this.props.arrange.isCenter) {
+      controllerUnitClassName += ' is-center';
+
+      // 如果同时对应的是翻转图片，显示控制按钮的翻转态
+      if (this.props.arrange.isInverse) {
+        controllerUnitClassName += ' is-inverse';
+      }
+    }
+    return (
+      <span className={controllerUnitClassName} onClick={this.handleClick.bind(this)}></span>
+      );
+  }
+}
+
 class GallleryByReactApp extends React.Component {
   constructor(props) {
     super(props);
@@ -155,7 +187,12 @@ class GallleryByReactApp extends React.Component {
       imageFigures.push(<ImgFigure key={index} data={value}
         ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]}
         inverse={this.inverse(index)} center={this.center(index)}
-        />)
+        />);
+
+      controllerUnits.push(<ControllerUnit key={index} arrange={this.state.imgsArrangeArr[index]}
+        inverse={this.inverse(index)} center={this.center(index)}
+        />);
+
     }.bind(this));
 
 
@@ -221,7 +258,7 @@ class GallleryByReactApp extends React.Component {
       vPosRangeX = vPosRange.x,
 
       imgsArrangeTopArr = [],
-      topImgNum = Math.floor(Math.random() * 2), //取一个或者不取
+      topImgNum = Math.floor(Math.random() * 2), //取一个或者不取, 向下取整 ceil向上取整
 
       topImgSpliceIndex = 0,
       imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
@@ -233,6 +270,8 @@ class GallleryByReactApp extends React.Component {
         rotate: 0,
         isCenter: true
       };
+
+      // debugger; // 在此处开始调试
 
       // 取出要布局上侧的图片的状态信息
       topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
